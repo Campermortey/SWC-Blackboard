1df1f5fc128b70e5550cdc1e3e032528a1373df9
campermortey <mortey@gmail.com>
2014-06-08 15:09:50 -0400

    Updated GetSectionsFor Stored Procedure
    Was not originally working so I adjusted it with sample data, changed
    the syntax, and passed in a different userId. Now having issues with
    converting to C# dto

--- Database/Stored Procedures/TeacherDashboard.sql
+++ Database/Stored Procedures/TeacherDashboard.sql
@@ -9,11 +9,15 @@ CREATE PROCEDURE GetSectionsFor
 
 --selects the tables 
 SELECT  c.CourseName, s.SectionId, count(sc.StudentId) as StudentCount, s.IsArchived
-FROM Section s
+FROM Teacher 
+INNER JOIN Section s
+ON teacher.TeacherId = s.TeacherId
 INNER JOIN Course c
 ON s.CourseId = c.CourseId
 INNER JOIN StudentSection sc
 ON s.SectionId = sc.SectionId
+WHERE UserId = @UserId
 GROUP BY c.CourseName, s.SectionId, s.IsArchived
 
-EXEC GetSectionsFor @UserId = '064df794-b4e7-41ed-ba1e-529cec36d924';
+
+EXEC GetSectionsFor @UserId = '750621f5-cfdd-4ee9-b55a-4ada1a33d9fb';

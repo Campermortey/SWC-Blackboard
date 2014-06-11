c00ab773f66cde3af6a9989106b1c3c3fc56335f
campermortey <mortey@gmail.com>
2014-06-11 17:48:22 -0400

    Teacher Roster Update
    Number of students in course did not take into account Roster.IsDeleted.
    There should only be students that are not deleted, not all students

--- Database/StoredProcedures/TeacherDashboard.sql
+++ Database/StoredProcedures/TeacherDashboard.sql
@@ -1,13 +1,13 @@
 USE SWC_LMS
 GO
 
-CREATE PROCEDURE ClassSummaryGetList
+ALTER PROCEDURE ClassSummaryGetList
 (
 	@UserId nvarchar(128)
 ) AS
 
 SELECT c.ClassId, c.Name, c.IsArchived, 
-	(SELECT count(*) FROM Roster WHERE ClassId = c.ClassId) as NumberOfStudents
+	(SELECT count(*) FROM Roster WHERE ClassId = c.ClassId AND roster.IsDeleted=0) as NumberOfStudents
 FROM Class c
 WHERE c.UserId = @UserId
 ORDER BY c.Name, c.ClassId, c.IsArchived

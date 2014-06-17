4729563d804d784a8149eca897794e5d77e53b81
campermortey <mortey@gmail.com>
2014-06-17 11:19:36 -0400

    DTO for Gradebook view
    Added DTO for Gradebook View. Contains many different classes to drill
    down for data

--- ManagementSystem/LMS_mastery.Models/DTOsView/GradebookView.cs
+++ ManagementSystem/LMS_mastery.Models/DTOsView/GradebookView.cs
@@ -6,12 +6,37 @@ using System.Threading.Tasks;
 
 namespace LMS_mastery.Models.DTOsView
 {
+    // Main model for the Gradebook View
     public class GradebookView
     {
+        public string CourseName { get; set; }
+        public List<GradebookAssignment> Assignments { get; set; }
+        public List<GradebookStudent> Students { get; set; }
+    }
+
+    // Each assignment has an Id and a name
+    public class GradebookAssignment
+    {
+        public int AssignmentId { get; set; }
+        public string Name { get; set; }
+    }
+
+    // Each student has a list of assignments with scores
+    public class GradebookStudent
+    {
+        public int RosterId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public char LetterGrade { get; set; }
-        public string Name { get; set; }
-        public decimal Percent { get; set; }
+        public List<GradebookStudentGrade> Grades { get; set; }
+    }
+
+    // Assignments with points, scores, and points possible per RosterId (student)
+    public class GradebookStudentGrade
+    {
+        public int RosterId { get; set; }
+        public int AssignmentId { get; set; }
+        public int? Points { get; set; }
+        public decimal? Score { get; set; }
+        public int PointsPossible { get; set; }
     }
 }
